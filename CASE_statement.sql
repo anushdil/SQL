e4b9f85fb76f119ef38322d41153ee6c4a8743e6
anushdil <dilshanigp@gmail.com>
2024-06-11 16:29:13 +0800

    chng

--- CASE_statement.sql
+++ CASE_statement.sql
@@ -1,7 +1,7 @@
 select mangr_name, location_code, 
 count(CASE WHEN sales_account_type='01' and voice='Y' THEN mobile_no END ) as type_A,
-count(CASE WHEN sales_account_type='02'  and voice='Y' THEN mobile_no END ) as type_B,
-count(CASE WHEN sales_account_type='06'  and voice='Y' THEN mobile_no END ) as type_C,
+count(CASE WHEN sales_account_type='05'  and voice='Y' THEN mobile_no END ) as type_B,
+count(CASE WHEN sales_account_type='09'  and voice='Y' THEN mobile_no END ) as type_C,
 count(CASE WHEN sales_account_type='02'  and voice='N' THEN mobile_no END ) as type_D,
 from product_det  
 group by mangr_name,location_code   
